f6b8493153061ca3cbf007d382d3e28106910200
Loren-4flag <ronaldojr873@gmail.com>
2024-12-27 22:31:50 +0000

    Update config.js

--- config.js
+++ config.js
@@ -26,23 +26,23 @@ function convertToBool(text, fault = 'true') {
 //--------------------------Mr Frank---------------------------------------
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SUBZERO SESSION HERE",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
-OWNER_NUMBER: process.env.OWNER_NUMBER || "242067274660",
-MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
+SESSION_ID: process.env.SESSION_ID || "GYA0XDYQ#i-Rbmx3CFJ0vNnBuTyJ-TYVPVlWcQdpFFbDE0_yFvJ8",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", //TRUE OR FALSE
+OWNER_NUMBER: process.env.OWNER_NUMBER || "221709710224",
+MODE: process.env.MODE || "private",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/cLL0MWyT/JINHUYK-MD-V2.jpg", // DO NOT CHANGE
 ALIVE_MSG: process.env.ALIVE_MSG || "𝙹𝙸𝙽𝙷𝚄𝚈𝙺 ᴀɪ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *𝚂𝙰𝚂𝙰𝙺𝙸-𝙲𝙾𝙼𝙿𝙰𝙶𝙽𝙸𝙴🍂*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ 𝙺𝙰𝙽𝙶 𝙹𝙸𝙽𝙷𝚄𝚈𝙺",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || ".", // .,!/?
+PREFIX: process.env.PREFIX || &
 FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
-HEART_REACT: process.env.HEART_REACT || "false",
-OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 ＪＩＮＨＵＹＫ-ＭＤ-Ｖ２ 』",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+HEART_REACT: process.env.HEART_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "true",
+BOT_NAME: process.env.BOT_NAME || "『 lorenzo-ＭＤ-Ｖ２ 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 
